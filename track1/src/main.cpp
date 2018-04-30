173bdcfd3c73a030c0762794c555f8c1532ea005
Thorsten von Eicken <tve@voneicken.com>
2018-04-30 15:18:39 -0700

    parse nmea

--- track1/src/main.cpp
+++ track1/src/main.cpp
@@ -1,14 +1,27 @@
 // Show a map of devices found on the I2C bus.
 
 #include <jee.h>
+#include <jee/nmea.h>
 
-UartDev< PinA<9>, PinA<10> > console;
-UartDev< PinA<2>, PinA<3> > gps;
+UartBufDev< PinA<9>, PinA<10> > console;
 
 int printf(const char* fmt, ...) {
     va_list ap; va_start(ap, fmt); veprintf(console.putc, fmt, ap); va_end(ap);
     return 0;
 }
+
+#if 0
+UartDev< PinA<2>, PinA<3> > gps_uart;
+#else
+UartBufDev< PinA<2>, PinA<3>, 85 > gps_uart;
+#endif
+
+int gps_printf(const char* fmt, ...) {
+    va_list ap; va_start(ap, fmt); veprintf(gps_uart.putc, fmt, ap); va_end(ap);
+    return 0;
+}
+
+NMEA nmea;
 
 I2cBus< PinB<7>, PinB<6> > bus;  // standard I2C pins for SDA and SCL
 PinA<15> led;
@@ -31,6 +44,7 @@ void detectI2c (T bus) {
 }
 
 int main () {
+    console.init();
     printf("\r\n===== marine tracker v1 starting ====\r\n\n");
     led.mode(Pinmode::out);
     led = 0;
@@ -41,21 +55,28 @@ int main () {
 
     detectI2c(bus);
 
-    printf("printing GPS\r\n");
-
     // switch uart baud rate to 9600
-    gps.baud(9600, hz);
-    printf("gps.cr1=0x%x\r\n", MMIO32(gps.cr1));
-    printf("gps.brr=%d\r\n", MMIO32(gps.brr));
-    printf("console.brr=%d\r\n", MMIO32(console.brr));
-    printf("ccipr=0x%x\r\n", MMIO32(Periph::rcc+0x4C));
+    printf("setting up GPS\r\n");
+    gps_uart.init();
+    gps_uart.baud(9600, hz);
+    wait_ms(100);
+    gps_printf("\r\n");
+    gps_printf("$PMTK605*31\r\n");
 
-    for (int i=0; i<20; i++) gps.putc('A');
     printf("printing GPS\r\n");
-
     while (1) {
-        uint8_t ch = gps.getc();
+        uint8_t ch = gps_uart.getc();
         console.putc(ch);
+#if 1
+        bool fix = nmea.parse(ch);
+        if (fix) {
+            printf("\r\n20%02d-%02d-%02d %02d:%02d:%02d.%03d %d.%04dN/S %d.%04dE/W %d.%02dkn %d.%02ddeg\r\n",
+                    nmea.date%100, nmea.date/100%100, nmea.date/10000,
+                    nmea.time/100, nmea.time%100, nmea.msecs/1000, nmea.msecs%1000,
+                    nmea.lat/10000, nmea.lat%10000, nmea.lon/10000, nmea.lon%10000,
+                    nmea.knots/100, nmea.knots%100, nmea.course/100, nmea.course%100);
+        }
+#endif
     }
 
 }
